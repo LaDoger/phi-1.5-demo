e7f651cb2ac148c32595d3c819f2394b029badc6
LaDoger <60722582+LaDoger@users.noreply.github.com>
2023-11-09 14:22:53 +0800

    Add files via upload

--- infer.py
+++ infer.py
@@ -5,7 +5,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 def main():
 
     # Comment this out if you don't have a good Nvidia GPU
-    # torch.set_default_device("cuda")
+    torch.set_default_device('cuda')
 
     # Can change your model here
     model_name = 'microsoft/phi-1_5'
@@ -14,19 +14,19 @@ def main():
 
     model = AutoModelForCausalLM.from_pretrained(
         model_name,
-        trust_remote_code=True
+        trust_remote_code=True,
+        torch_dtype=torch.bfloat16
     )
     tokenizer = AutoTokenizer.from_pretrained(
         model_name,
-        trust_remote_code=True
+        trust_remote_code=True,
+        torch_dtype=torch.bfloat16
     )
 
     print('\nModel loaded! Please start prompting!')
 
-    while True:
-        prompt = input('----------------\nPrompt: ')
-
-        print('\nInferring...')
+    def infer(prompt, max_length) -> str:
+        """Returns predicted words AFTER the prompt text"""
 
         inputs = tokenizer(
             prompt,
@@ -34,12 +34,29 @@ def main():
             return_attention_mask=False
         )
 
-        # Change max_length to set output token amount
-        outputs = model.generate(**inputs, max_length=32)
+        outputs = model.generate(**inputs, max_length=max_length)
         text = tokenizer.batch_decode(outputs)[0]
 
-        print(f'\nOutput: {text}')
+        return text[len(prompt):]
+    
 
+    while True:
+        prompt = input('----------------\nPrompt: ')
+        prompt_token_count = len(tokenizer.tokenize(prompt))
+        
+        print(f'\nOutput: {prompt}', end='')
+
+        # Change this to change how many tokens you want to generate
+        for i in range(100): # Generate 100 tokens
+
+            next_word = infer(prompt, max_length=prompt_token_count + 1)
+            print(next_word, end='', flush=True)
+            
+            prompt += next_word
+            prompt_token_count += 1
+        
+        print('')
+            
 
 if __name__ == '__main__':
     main()
